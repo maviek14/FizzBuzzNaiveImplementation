b6b4d560ff9a6f6096c6395adac813f40db5c0f1
maviek14 <mt82032@stud.uph.edu.pl>
2023-11-06 22:25:29 +0100

    added FizzBuzz class

---
+++ FizzBuzz.cs
@@ -0,0 +1,18 @@
+﻿namespace NaiveFizzBuzz;
+
+public class FizzBuzz
+{
+    public static string ProcessNumber(int number)
+    {
+        string result = string.Empty;
+
+        if (number % 3 == 0)
+            result += "Fizz";
+        if (number % 5 == 0)
+            result += "Buzz";
+        if (result.Length == 0)
+            result = number.ToString();
+
+        return result;
+    }
+}
