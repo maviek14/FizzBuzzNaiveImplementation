784182d9cb666d26c0f0982ec4c5a335945990bc
maviek14 <mt82032@stud.uph.edu.pl>
2023-11-06 22:25:29 +0100

    added FizzBuzz class
    modified ProcessNumber class

---
+++ FizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿namespace NaiveFizzBuzz;
+
+public class FizzBuzz
+{
+    public static string ProcessNumber(int number)
+    {
+        string result = string.Empty;
+
+        if(IsDivisibleBy(number, 3))
+            result += "Fizz";
+        if (IsDivisibleBy(number, 5))
+            result += "Buzz";
+        if (result.Length == 0)
+            result = number.ToString();
+
+        return result;
+    }
+
+    private static bool IsDivisibleBy(int number, int divider)
+    {
+        return number % divider == 0;
+    }
+}
